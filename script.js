38e1946159a91bd2bb444e6ae80655c91651be92
KAIKE BARROS <kaikebarros32@gmail.com>
2025-06-10 11:37:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let particula = document.querySelector(".particula")
+let imagem = document.querySelector(".mouse")
+
+function mudarCor(cor){
+
+particula.style.background = cor
+
+
+   
+
+}
+
+
+
+
+function mudarImagem(img){
+    imagem.src = img
+}
